e0ab7f5e8efb88851be3b498b1ba7776b5ef2ae5
SanjayKumar27 <sanjaykumarar3@gmail.com>
2025-07-25 11:29:21 +0530

    Recoil with atom,selector,memo

--- week11/Recoil/src/App.jsx
+++ week11/Recoil/src/App.jsx
@@ -1,41 +1,99 @@
-import React, { createContext, useContext, useState } from 'react';
-import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil';
+
+// import { RecoilRoot, useSetRecoilState, useRecoilValue } from 'recoil';
+// import './App.css'
+// import { counterAtom } from './store/atoms/counter';
+
+
+// function App(){
+//   return <div>
+//       <RecoilRoot>
+//       <Counter/>
+//       </RecoilRoot>
+  
+//   </div>
+// }
+// function Counter(){
+  
+//   return <div>
+//     <CurrentCount/>
+//     <Increase />
+//     <Decrease/>
+//   </div>
+// }
+
+// function CurrentCount(){
+//   const count=useRecoilValue(counterAtom);
+//   return <div>
+//     {count}
+//   </div>
+// }
+
+
+// function Increase(){
+//  const Setcount=useSetRecoilState(counterAtom);
+  
+//   return <div>
+//     <button onClick={()=>Setcount(c=>c+1)}>Increase</button>
+//   </div>
+// }
+
+
+// function Decrease(){
+//   const Setcount=useSetRecoilState(counterAtom);
+  
+//   return <div>
+//     <button onClick={()=>Setcount(c=>c-1)}>Decrease</button>
+//   </div>
+// }
+// export default App;
+
+//Memo in react 
+//In react if parent component re render then all the child components also rerender
+// even though there is no state/prop is not passed the child component rerendere
+
+//by warping memo function around the component make children component only rerender when there is a 
+// change in the prop change or state chage occur only then the componet is rerendered
+
+
+//Selector in the recoil 
+//selector is drived state of the atom which can only read atom and only rerender is the derive part of the 
+//atom in the selecctor is changed
+import {RecoilRoot, useRecoilValue,useSetRecoilState} from "recoil";
+import { counterAtom, iseven } from "./store/atoms/counter";
 import './App.css'
-const count = atom({
-  key: 'countState', // unique ID (with respect to other atoms/selectors)
-  default: 0, // default value (aka initial value)
-});
+function App(){
 
-function Parent() {
-  return (
-    <RecoilRoot>
-      <Incrase />
-      <Decrease />
-      <Value />
-    </RecoilRoot>
-  );
-}
-
-function Decrease() {
-  const setCount = useSetRecoilState(count);
-  return <button onClick={() => setCount(count => count - 1)}>Decrease</button>;
-}
-
-function Incrase() {
-  const setCount = useSetRecoilState(count);
-  return <button onClick={() => setCount(count => count + 1)}>Increase</button>;
-}
-
-function Value() {
-  const countValue = useRecoilValue(count);
-  return <p>Count: {countValue}</p>;
-}
-
-// App Component
-const App = () => {
   return <div>
-    <Parent />
+    <RecoilRoot>
+        <Buttons/>
+        <Counter/>
+        <IsEven/>
+    </RecoilRoot>
   </div>
-};
+}
+
+function Buttons()
+{
+  const SetCount=useSetRecoilState(counterAtom);
+  return <div>
+    <button onClick={()=>SetCount(c=>c+2)}>Increase</button>
+    <button onClick={()=>SetCount(c=>c-1)}>Decrease</button>
+  </div>
+}
+
+function Counter(){
+  const count=useRecoilValue(counterAtom);
+  return <div>
+    {count}
+  </div>
+}
+
+function IsEven(){
+  const even=useRecoilValue(iseven);
+
+  return <div>
+    {even ? "Even":"Odd"}
+  </div>
+}
 
 export default App;
--- week11/Recoil/src/store/atoms/counter.js
+++ week11/Recoil/src/store/atoms/counter.js
@@ -1,5 +1,16 @@
-import {atom} from "recoil"
+import {atom,selector} from "recoil"
 export const counterAtom=atom({
-    key:"counterAtom",
+    key:"count",
     default:0
 })
+
+export const iseven=selector(
+    {
+        key:"isEven",
+        get:({get})=>{
+            const count=get(counterAtom);
+            const iseven=(count %2===0);
+            return iseven;
+        }
+    }
+)
